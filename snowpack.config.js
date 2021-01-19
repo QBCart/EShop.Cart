7cec25739d4769149580f87ca772c137213b9348
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-01-19 12:01:28 -0500

    added snowpack config

---
+++ snowpack.config.js
@@ -0,0 +1,15 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    mount: {
+      src: {url: '/', resolve: true, static: false},
+    },
+    devOptions: {
+      open: "none"
+    },
+    buildOptions: {
+      clean: true,
+    },
+    plugins: [
+      ['@snowpack/plugin-sass', { compilerOptions: { loadPath: 'node_modules'}}]
+    ]
+  };
