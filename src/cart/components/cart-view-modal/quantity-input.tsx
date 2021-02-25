fa470ce904d1ddb9d57046f1cd4e711de64e8bf2
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-02-25 10:23:23 -0500

    added quantity input component

---
+++ src/cart/components/cart-view-modal/quantity-input.tsx
@@ -0,0 +1,61 @@
+import React, {
+  ChangeEvent,
+  FC,
+  useState
+} from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js';
+
+interface Props {
+  quantity: number;
+  itemId: string;
+  changeItemQuantity(e: any): void;
+}
+
+const QuantityInput: FC<Props> = (props) => {
+  const [updateReady, setUpdateReady] = useState(false);
+
+  const changeItemInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.currentTarget.value;
+    const inputValueNum = Number(inputValue);
+    if (
+      typeof inputValueNum === 'number' &&
+      inputValueNum > 0 &&
+      inputValueNum % 1 === 0 &&
+      inputValueNum !== props.quantity
+    ) {
+      setUpdateReady(true);
+    } else {
+      setUpdateReady(false);
+    }
+  };
+
+  return (
+    <div>
+      {console.log('rendered')}
+      <label>Quantity: </label>
+      <input
+        id={`qty-input-${props.itemId}`}
+        onChange={changeItemInputValue}
+        // data-id={itemId}
+        // onBlur={props.revertItemInputValue}
+        type="number"
+        min="1"
+        defaultValue={props.quantity}
+        className="quantity-input-cart form-control-sm ml-2 mr-1"
+      ></input>
+      {updateReady ? (
+        <button
+          data-id={props.itemId}
+          className="btn btn-success cart-quantity-update"
+          onClick={(e) => {
+            setUpdateReady(false);
+            props.changeItemQuantity(e);
+          }}
+        >
+          update
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+export default QuantityInput;
