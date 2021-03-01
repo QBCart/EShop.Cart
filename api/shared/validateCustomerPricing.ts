64caf38b764b9a18534526d99fb3422c2a432d24
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-01 16:41:21 -0500

    initial validate stubs

---
+++ api/shared/validateCustomerPricing.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ * This code can only be used and/or distributed with express permission.
+ */
+ 
+ export default async function validateCustomerPricing(cart: any) {
+ }
