ac50050a8e62a4c06b65f3b5bbc479794d97047d
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-01 16:42:33 -0500

    added initial logic

--- api/validate/index.ts
+++ api/validate/index.ts
@@ -1,17 +1,35 @@
 import type { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import type { User } from "@qbcart/types";
+import validateCart from "../shared/validateCart";
+import validateCustomerPricing from "../shared/validateCustomerPricing";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+    try {
+        const user: User = req.body?.user;
+        //! TEMPORARY DEV ENVIRONMENT SWITCH - REMOVE BEFORE RELEASE// 
+        const cart: any = req.body?.data || req.body;
+            await validateCart(cart);
+        if (user) {
+          await validateCustomerPricing(cart);
+        }
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: cart,
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+      } catch (error) {
+        console.error(error);
+        context.res = {
+          status: '500',
+          body:
+            'Contact Support: The Search API seems to be having internal issues.',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+      }
 };
 
 export default httpTrigger;
