cbf04f3a6874d39d95d2907aaa046499be9fb287
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-01 16:43:35 -0500

    added initial logic

--- api/get/index.ts
+++ api/get/index.ts
@@ -1,17 +1,37 @@
 import type { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import type { User } from '@qbcart/types'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+    try {
+        const user: User = req.body?.user;
+        if (user) {
+          context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: null,
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+        } else {
+          context.res = {
+            status: '401',
+            body: 'Error: Unauthorized.',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+        }
+      } catch (error) {
+        console.error(error);
+        context.res = {
+          status: '500',
+          body:
+            'Contact Support: The Search API seems to be having internal issues.',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+      }
 };
 
 export default httpTrigger;
