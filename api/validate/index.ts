a4c44845725dd712a9cc1e2405cae2b63c6b00b7
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-03 09:26:38 -0500

    typed cart

--- api/validate/index.ts
+++ api/validate/index.ts
@@ -1,5 +1,6 @@
 import type { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import type { User } from '@qbcart/types';
+import type { EShopCart } from '@qbcart/types/eshop';
 import validateCart from '../shared/validateCart';
 import validateCustomerPricing from '../shared/validateCustomerPricing';
 
@@ -8,9 +9,9 @@ const httpTrigger: AzureFunction = async function (
   req: HttpRequest
 ): Promise<void> {
   try {
-    const user: User = req.body?.user;
-    //! TEMPORARY DEV ENVIRONMENT SWITCH - REMOVE BEFORE RELEASE//
-    const cart: any = req.body?.data || req.body;
+    const isDevEnv = process.env.AZURE_FUNCTIONS_ENVIRONMENT === 'Development';
+    const user: User = isDevEnv ? { oid: '1111' } : req.body?.user;
+    const cart: EShopCart = isDevEnv ? req.body?.data || req.body : req.body?.data;
     await validateCart(cart);
     if (user) {
       await validateCustomerPricing(cart);
