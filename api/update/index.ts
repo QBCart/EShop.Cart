f8a46365c41a6072da7a7bfbdb64796484c6c9e4
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-03 09:22:30 -0500

    updated api usage

--- api/update/index.ts
+++ api/update/index.ts
@@ -1,6 +1,8 @@
 import type { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import type { User } from '@qbcart/types';
+import type { EShopCart } from '@qbcart/types/eshop';
 import cosmos from '../shared/cosmos';
+
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -10,26 +12,30 @@ const httpTrigger: AzureFunction = async function (
     const isDevEnv = process.env.AZURE_FUNCTIONS_ENVIRONMENT === 'Development';
     const user: User = isDevEnv ? { oid: '1111' } : req.body?.user;
     if (user) {
-      const cart: any = isDevEnv ? req.body?.data || req.body : req.body?.data;
-      // TODO: update backend cart
-      cosmos.customers.container.items.upsert({
-        id: `ESHOP-CART-${user.oid}`,
-        Discriminator: 'EShopCart',
-        Items: cart.items,
-        LastUpdated: cart.lastUpdated
-      });
-      context.res = {
-        // status: 200, /* Defaults to 200 */
-        //
-        body: {},
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
+      const cart: EShopCart = isDevEnv ? req.body?.data || req.body : req.body?.data;
+      cart.id = `ESHOP-CART-${user.oid}`;
+      if(await cosmos.customers.upsertEshopCart(cart)) {
+        context.res = {
+          // status: 200, /* Defaults to 200 */
+          //
+          body: {},
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+      } else {
+        context.res = {
+          status: '417',
+          body: 'Cart Update API Error: Failed To Update.',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        };
+      }
     } else {
       context.res = {
         status: '401',
-        body: 'Cart API Error: Unauthorized.',
+        body: 'Cart Update API Error: Unauthorized.',
         headers: {
           'Content-Type': 'application/json'
         }
@@ -39,7 +45,7 @@ const httpTrigger: AzureFunction = async function (
     console.error(error);
     context.res = {
       status: '500',
-      body: 'Cart Get API Error: Internal Server Issues.',
+      body: 'Cart Update API Error: Internal Server Issues.',
       headers: {
         'Content-Type': 'application/json'
       }
