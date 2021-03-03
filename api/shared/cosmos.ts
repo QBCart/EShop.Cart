e3f34d5e2c6baff2a55abd55c5ff55b3d44e048e
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-03 08:46:57 -0500

    format

--- api/shared/cosmos.ts
+++ api/shared/cosmos.ts
@@ -4,14 +4,14 @@
  * This code can only be used and/or distributed with express permission.
  */
 
- import { Cosmos } from '@qbcart/cosmos';
+import { Cosmos } from '@qbcart/cosmos';
 
- const cosmos = new Cosmos(
-   process.env.cosmosConnectionString!,
-   process.env.cosmosDatabase!,
-   process.env.cosmosInfoContainer!,
-   process.env.cosmosItemsContainer!,
-   process.env.cosmosCustomersContainer!
- );
- 
- export default cosmos;
+const cosmos = new Cosmos(
+  process.env.cosmosConnectionString!,
+  process.env.cosmosDatabase!,
+  process.env.cosmosInfoContainer!,
+  process.env.cosmosItemsContainer!,
+  process.env.cosmosCustomersContainer!
+);
+
+export default cosmos;
