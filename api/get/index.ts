11530f16424c18cdcf78abd11d1413926cf0d91d
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-03 09:02:58 -0500

    typed call for cart

--- api/get/index.ts
+++ api/get/index.ts
@@ -1,5 +1,6 @@
 import type { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import type { User } from '@qbcart/types';
+import type { EShopCart } from '@qbcart/types/eshop';
 import cosmos from '../shared/cosmos';
 import validateCart from '../shared/validateCart';
 
@@ -13,9 +14,8 @@ const httpTrigger: AzureFunction = async function (
         ? { oid: '1111' }
         : req.body?.user;
     if (user) {
-      const cart = await cosmos.customers.read<any>(
-        `ESHOP-CART-${user.oid}`,
-        'EShopCart'
+      const cart = await cosmos.customers.getEshopCart(
+        `ESHOP-CART-${user.oid}`
       );
       if (cart) {
         await validateCart(cart);
@@ -23,8 +23,8 @@ const httpTrigger: AzureFunction = async function (
           // status: 200, /* Defaults to 200 */
           //
           body: {
-            items: cart.Items,
-            lastUpdated: cart.LastUpdated
+            items: cart.items,
+            lastUpdated: cart.lastUpdated
           },
           headers: {
             'Content-Type': 'application/json'
