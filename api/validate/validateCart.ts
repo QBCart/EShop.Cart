43ae92cec2ee261969fda6e3be94eeafb284161d
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-04 09:52:45 -0500

    file restructure
    Co-authored-by: Allan Mobley <allan.mobley.jr@qbcart.net>

--- api/validate/validateCart.ts
+++ api/validate/validateCart.ts
@@ -4,9 +4,9 @@
  * This code can only be used and/or distributed with express permission.
  */
 
-import type { EShopCart } from '@qbcart/types/eshop';
+import type CartItems from '@qbcart/types/types/internals/cart-items';
 
-export default async function validateCart(cart: EShopCart) {
+export default async function validateCart(cart: CartItems) {
   // for (let itemId in cart.items) {
   //     cart.items[itemId].salesDesc = 'Hello World'
   // }
