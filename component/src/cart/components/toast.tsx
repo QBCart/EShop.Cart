ffca3a514a5d51739e242ab4e3e9ae969fa9aa14
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-04 16:07:21 -0500

    format

--- component/src/cart/components/toast.tsx
+++ component/src/cart/components/toast.tsx
@@ -6,65 +6,64 @@
 
 // React import is needed by snowpack even though typescript suggests otherwise
 import React, {
-    FC,
-    forwardRef,
-    useEffect
-  } from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js';
-  
-  interface Props {
-    imagesStorageUrl: string;
-  }
-  
-  const NetworkToast = forwardRef<HTMLDivElement, Props>((props, ref) => {
-    useEffect(() => {
-      $('#qbc-eshop-cart-toast').toast({
-        delay: 3000
-      });
-    }, []);
+  FC,
+  forwardRef,
+  useEffect
+} from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js';
 
-    useEffect(() => {
-        $('#qbc-eshop-cart-toast').on('hidden.bs.toast', function () {
-            const toastBody = $('#qbc-eshop-cart-toast .toast-body');
-            toastBody.text('');
-            toastBody.removeClass(['text-success', 'text-danger']);
-          })
-      }, []);
-  
-    return (
+interface Props {
+  imagesStorageUrl: string;
+}
+
+const NetworkToast = forwardRef<HTMLDivElement, Props>((props, ref) => {
+  useEffect(() => {
+    $('#qbc-eshop-cart-toast').toast({
+      delay: 3000
+    });
+  }, []);
+
+  useEffect(() => {
+    $('#qbc-eshop-cart-toast').on('hidden.bs.toast', function () {
+      const toastBody = $('#qbc-eshop-cart-toast .toast-body');
+      toastBody.text('');
+      toastBody.removeClass(['text-success', 'text-danger']);
+    });
+  }, []);
+
+  return (
+    <div
+      id="qbc-eshop-cart-toast-container"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
       <div
-        id="qbc-eshop-cart-toast-container"
+        id="qbc-eshop-cart-toast"
+        className="toast"
+        role="alert"
         aria-live="assertive"
         aria-atomic="true"
       >
-        <div
-          id="qbc-eshop-cart-toast"
-          className="toast"
-          role="alert"
-          aria-live="assertive"
-          aria-atomic="true"
-        >
-          <div className="toast-header">
-            <img
-              src={`${props.imagesStorageUrl}images/favicon.ico`}
-              alt="company logo"
-              width="18"
-            />
-            <strong className="ml-1 mr-auto">Cart</strong>
-            <small>Just now</small>
-            <button
-              type="button"
-              className="ml-2 mb-1 close"
-              data-dismiss="toast"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="toast-body" ref={ref}></div>
+        <div className="toast-header">
+          <img
+            src={`${props.imagesStorageUrl}images/favicon.ico`}
+            alt="company logo"
+            width="18"
+          />
+          <strong className="ml-1 mr-auto">Cart</strong>
+          <small>Just now</small>
+          <button
+            type="button"
+            className="ml-2 mb-1 close"
+            data-dismiss="toast"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
+        <div className="toast-body" ref={ref}></div>
       </div>
-    );
-  });
-  
-  export default NetworkToast;
-  
+    </div>
+  );
+});
+
+export default NetworkToast;
