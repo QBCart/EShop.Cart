73f371379938398a4daac045271fd09f6c53ef4a
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-04 16:10:44 -0500

    working on cart flow logic

--- component/src/cart/index.tsx
+++ component/src/cart/index.tsx
@@ -9,6 +9,7 @@ import type ProductModalItem from '@qbcart/eshop-product-modal/types/product-mod
 import CartViewModal from './components/cart-view-modal';
 import ClearCartModal from './components/clear-cart-modal';
 import ClearItemModal from './components/clear-item-modal';
+import Toast from './components/toast';
 
 import type CartState from './types/CartState';
 import type { EShopCart } from '@qbcart/types/eshop';
@@ -20,13 +21,9 @@ interface Props {
 }
 
 const Cart: FC<Props> = (props) => {
-  const initCartState = (
-    useCurrentTime?: boolean,
-    ignoreGuestCart?: boolean
-  ) => {
+  const initCartState = (ignoreGuestCart?: boolean) => {
     return {
       items: {},
-      lastUpdated: useCurrentTime ?? false ? Date.now() : 0,
       ignoreGuestCart: ignoreGuestCart ?? false
     } as CartState;
   };
@@ -49,58 +46,84 @@ const Cart: FC<Props> = (props) => {
   };
 
   const [cart, setCart] = useState<CartState>(initCartFromLocalStorage());
+  const [lastPulledFromLocalStorage, setLastPulledFromLocalStorage] = useState(0);
+  const [toastBody] = useState(React.createRef<HTMLDivElement>());
+  const [startTime] = useState(Date.now());
+  
 
   useEffect(() => {
-    console.log('effect: initUseEffects');
-    initUseEffects();
+    validateCart();
   }, []);
 
-  // Persist CartState
-  useEffect(() => {
-    updateLocalStorage(cart);
-    // TODO: Prevent updateBackend on init with stale user cart
-    if (props.userLoggedIn && cart.lastUpdated > initCartState().lastUpdated) {
-      updateBackendCart();
-    }
-    console.log('effect: local storage');
-  }, [cart.lastUpdated]);
+  // useEffect(() => {
+  //   updateLocalStorage(cart);
+  // }, [cart]);
 
-  const initUseEffects = async () => {
-    await validateCart();
-    if (props.userLoggedIn) {
-      getBackendCart();
+  useEffect(() => {
+    showToast();
+  }, [cart]);
+
+  const showToast = (message?: string) => {
+    if (message) {
+      toastBody.current!.textContent = message;
+    }
+    if (toastBody.current!.textContent) {
+      $('#qbc-eshop-cart-toast').toast('show');
+    }
+  };
+
+  const checkUptime = () => {
+    const currentTime = Date.now();
+    const twoHoursAsMilliseconds = 7200000;
+    // const twoHoursAsMilliseconds = 20000;
+    if (currentTime - startTime > twoHoursAsMilliseconds) {
+      // TODO Creat modal to prompt the user to update
+      showToast(
+        'This tab has been open for more than 2 hours. You may want to refresh your page'
+      );
     }
   };
 
   const validateCart = async () => {
-    if (Object.keys(cart.items).length > 0) {
-      try {
-        const res = await fetch(
-          `${props.cartAPI ? props.cartAPI + '/validate' : '/cart/validate'}`,
-          {
-            credentials: 'include',
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Credentials': 'true'
-            },
-            body: JSON.stringify(cart)
+    if (props.userLoggedIn || Object.keys(cart.items).length > 0) {
+      let retry = 3;
+      while (retry--) {
+        try {
+          const res = await fetch(
+            `${props.cartAPI ? props.cartAPI + '/validate' : '/cart/validate'}`,
+            {
+              credentials: 'include',
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': 'true'
+              },
+              body: JSON.stringify(cart.items)
+            }
+          );
+          if (res.ok) {
+            const results = await res.json();
+            if (results.removedItems.length > 0) {
+              // TODO: create & populate removed items modal
+            }
+            setCart({
+              items: results.items,
+              ignoreGuestCart: cart.ignoreGuestCart
+            });
+            break;
+          } else {
+            if (!retry) {
+              showToast(await res.text());
+            }
           }
-        );
-        if (res.ok) {
-          const validatedCart: CartState = await res.json();
-          updateLocalStorage(validatedCart);
-          setCart(validatedCart);
-        } else {
-          // TODO: Add network toast
-          console.error(await res.text());
+        } catch (error) {
+          showToast(
+            `Our systems are having trouble validating your cart online. Please contact support.`
+          );
+          break;
         }
-      } catch (error) {
-        // TODO: Add network toast
-        console.error('Try/Catch Error: ' + error);
       }
     }
-    console.log('effect: validateCart');
   };
 
   const getBackendCart = async () => {
@@ -113,86 +136,66 @@ const Cart: FC<Props> = (props) => {
         }
       );
       if (res.ok) {
-        lastCartWins(await res.json());
+        return {
+          items: await res.json(),
+          ignoreGuestCart: cart.ignoreGuestCart
+        } as CartState;
       } else {
-        if (res.status !== 404) {
-          // TODO: Add network toast
-          console.error(await res.text());
+        switch (res.status) {
+          case 401:
+            throw await res.text();
+          case 500:
+            throw await res.text();
+          case 404:
+          default:
+            // no cart yet
+            return { ...cart };
         }
       }
     } catch (error) {
-      // TODO: Add network toast
-      console.error('Try/Catch Error: ' + error);
-    }
-
-    console.log('effect: getBackendCart');
-  };
-
-  const lastCartWins = (backendCart: EShopCart) => {
-    if (backendCart.lastUpdated > cart.lastUpdated) {
-      setCart({
-        items: backendCart.items,
-        lastUpdated: backendCart.lastUpdated,
-        ignoreGuestCart: cart.ignoreGuestCart
-      });
-    }
-  };
-
-  const updateBackendCart = async () => {
-    try {
-      const res = await fetch(
-        `${props.cartAPI ? props.cartAPI + '/update' : '/cart/update'}`,
-        {
-          credentials: 'include',
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            items: cart.items,
-            lastUpdated: cart.lastUpdated
-          })
-        }
-      );
-      if (!res.ok) {
-        console.log('Internet may be having issues.');
-      }
-    } catch (err) {
-      console.log(err);
-    }
+      throw "You're probably offline. Check your network.";
+    } 
   };
 
   const pullFromLocalStorage = () => {
-    if (props.userLoggedIn && localStorage.guestCart) {
-      if (localStorage.userCart) {
-        let newCart: CartState = JSON.parse(localStorage.userCart);
+    const lastTimeUpdated: number = Number(localStorage.lastTimeUpdated)
+    if ( lastTimeUpdated !== lastPulledFromLocalStorage) {
+      if (props.userLoggedIn && localStorage.guestCart) {
+        if (localStorage.userCart) {
+          let newCart: CartState = JSON.parse(localStorage.userCart);
 
-        if (newCart.ignoreGuestCart) {
-          console.log('ignore guest cart: ' + newCart.ignoreGuestCart);
-        } else {
-          console.log('would you like to merge guest cart to yours?');
-          newCart.ignoreGuestCart = true;
-          console.log('ignore guest cart: ' + newCart.ignoreGuestCart);
+          if (newCart.ignoreGuestCart) {
+            // console.log('ignore guest cart: ' + newCart.ignoreGuestCart);
+          } else {
+            // console.log('would you like to merge guest cart to yours?');
+            // newCart.ignoreGuestCart = true;
+            // console.log('ignore guest cart: ' + newCart.ignoreGuestCart);
+          }
+
+          setCart(newCart);
         }
 
+        // just a demo for what will soon be append guest cart logic
+      } else if (props.userLoggedIn && !localStorage.guestCart) {
+        if (localStorage.userCart) {
+          let newCart: CartState = JSON.parse(localStorage.userCart);
+          setCart(newCart);
+        }
+      } else if (!props.userLoggedIn && localStorage.guestCart) {
+        let newCart: CartState = JSON.parse(localStorage.guestCart);
         setCart(newCart);
+      } else {
+        return;
       }
 
-      // just a demo for what will soon be append guest cart logic
-    } else if (props.userLoggedIn && !localStorage.guestCart) {
-      if (localStorage.userCart) {
-        let newCart: CartState = JSON.parse(localStorage.userCart);
-        setCart(newCart);
-      }
-    } else if (!props.userLoggedIn && localStorage.guestCart) {
-      let newCart: CartState = JSON.parse(localStorage.guestCart);
-      setCart(newCart);
-    } else {
-      return;
+      setLastPulledFromLocalStorage(lastTimeUpdated)
+      console.log('pulled from local storage')
     }
   };
 
   const updateLocalStorage = (cart: CartState) => {
+    // localStorage.setItem('lastTimeUpdated', lastPulledFromLocalStorage.toString());
+    localStorage.setItem('lastTimeUpdated', Date.now().toString());
     if (props.userLoggedIn) {
       localStorage.setItem('userCart', JSON.stringify(cart));
     } else {
@@ -200,11 +203,13 @@ const Cart: FC<Props> = (props) => {
     }
   };
 
-  const addToCart = (item: ProductModalItem) => {
-    let newCart = { ...cart };
+  //! await an update from backend before allowing state update
+  const addToCart = async (item: ProductModalItem) => {
+    const newCart = { ...cart };
 
     if (newCart.items[item.id]) {
       newCart.items[item.id].quantity += item.quantity;
+      newCart.items[item.id].lastUpdated = Date.now();
     } else {
       newCart.items[item.id] = {
         id: item.id,
@@ -212,42 +217,105 @@ const Cart: FC<Props> = (props) => {
         salesDesc: item.salesDesc,
         salesPrice: item.salesPrice,
         href: item.href,
-        quantity: item.quantity
+        quantity: item.quantity,
+        lastUpdated: Date.now()
       };
     }
 
-    newCart.lastUpdated = Date.now();
+    toastBody.current!.textContent = `Item ${item.name} was added to the cart.`;
+    toastBody.current!.classList.add('text-success');
     setCart(newCart);
+    updateLocalStorage(newCart);
+
+    if (props.userLoggedIn) {
+      addToBackendCart(
+        item.id,
+        newCart.items[item.id].quantity,
+        newCart.items[item.id].lastUpdated
+      );
+    }
+  };
+
+  const addToBackendCart = async (
+    id: string,
+    quantity: number,
+    lastUpdated: number
+  ) => {
+    try {
+    } catch (error) {
+      // TODO: Set toast message with error and show
+      return false;
+    }
   };
 
   const changeItemQuantity = (id: string) => {
-    let newCart = { ...cart };
+    const newCart = { ...cart };
     newCart.items[id].quantity = parseInt(
       (document.getElementById(`qty-input-${id}`) as HTMLInputElement).value
     );
-    newCart.lastUpdated = Date.now();
+    newCart.items[id].lastUpdated = Date.now();
     setCart(newCart);
+    if (props.userLoggedIn) {
+      changeBackendItemQuantity(
+        id,
+        newCart.items[id].quantity,
+        newCart.items[id].lastUpdated
+      );
+    }
+  };
+
+  const changeBackendItemQuantity = async (
+    id: string,
+    quantity: number,
+    lastUpdated: number
+  ) => {
+    try {
+    } catch (error) {
+      // TODO: Set toast message with error and show
+      return false;
+    }
   };
 
   const clearItem = (id: string) => {
-    let newCart = { ...cart };
-    delete newCart.items[id];
-    newCart.lastUpdated = Date.now();
+    const newCart = { ...cart };
+    newCart.items[id].quantity = 0;
+    newCart.items[id].lastUpdated = Date.now();
     setCart(newCart);
+
+    if (props.userLoggedIn) {
+      clearBackendItem(id, newCart.items[id].lastUpdated);
+    }
   };
 
+  const clearBackendItem = async (id: string, lastUpdated: number) => {};
+
   const clearCart = () => {
-    const newCart: CartState = initCartState(true, cart.ignoreGuestCart);
+    const lastUpdated = Date.now();
+    const newCart = { ...cart };
+
+    for (let id in newCart.items) {
+      newCart.items[id].quantity = 0;
+      newCart.items[id].lastUpdated = lastUpdated;
+    }
     setCart(newCart);
+
+    if (props.userLoggedIn) {
+      clearBackendCart(lastUpdated);
+    }
   };
+
+  const clearBackendCart = async (lastUpdated: number) => {};
 
   return (
     <div>
+      <div>{lastPulledFromLocalStorage}</div>
+      <Toast imagesStorageUrl={props.imagesStorageUrl!} ref={toastBody} />
       <CartViewModal
         imagesStorageUrl={props.imagesStorageUrl}
         cartState={cart}
         pullFromLocalStorage={pullFromLocalStorage}
         changeItemQuantity={changeItemQuantity}
+        checkUptime={checkUptime}
       />
       <ProductModal
         addToCart={addToCart}
