7d6eb3593cded2e3d9b6629970df173596d5c8bb
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-15 09:37:57 -0400

    remove api
    Co-authored-by: Allan Mobley <allan-mobley-jr@users.noreply.github.com>

--- api/validate/validateCustomerPricing.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright (c) 2021 QBCart Inc. All rights reserved.
- * This code can only be used and/or distributed with express permission.
- */
-
-import type CartItems from '@qbcart/types/types/internals/cart-items';
-
-export default async function validateCustomerPricing(cart: CartItems) {}
