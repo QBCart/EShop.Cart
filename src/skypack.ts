499f15dc9159395b55fadc9e3e5fe4fc5a9bfacf
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-15 11:23:18 -0400

    export all

--- src/skypack.ts
+++ src/skypack.ts
@@ -4,11 +4,5 @@
  * This code can only be used and/or distributed with express permission.
  */
 
-export { useState, useEffect } from 'https://cdn.skypack.dev/react@v17.0.1?min';
-export type {
-  Dispatch,
-  SetStateAction,
-  FC,
-  ChangeEvent
-} from 'https://cdn.skypack.dev/react@v17.0.1?min';
-export { render } from 'https://cdn.skypack.dev/react-dom@v17.0.1?min';
+export * from 'https://cdn.skypack.dev/react@v17.0.1?min';
+export * from 'https://cdn.skypack.dev/react-dom@v17.0.1?min';
