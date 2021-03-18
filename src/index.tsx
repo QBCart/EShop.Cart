d6cedd803ce3fb1adc849aad1c021a35dfff157b
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-18 11:20:45 -0400

    updated mounting code

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,16 @@ const mountCart = (
   showToast: (header: string, body: string, duration: number) => void,
   cartAPI?: string
 ) => {
-  const cartDataSet = document.getElementById('qbc-eshop-cart')!.dataset;
+  const id = 'qbc-eshop-cart';
+  const mountingDiv = document.getElementById(id);
   render(
     <Cart
-      userLoggedIn={Boolean(cartDataSet.userLoggedIn)}
-      imagesStorageUrl={cartDataSet.imagesStorageUrl!}
+      userLoggedIn={Boolean(mountingDiv.dataset.userLoggedIn)}
+      imagesStorageUrl={mountingDiv.dataset.imagesStorageUrl!}
       showToast={showToast}
       cartAPI={cartAPI}
     />,
-    document.getElementById('qbc-eshop-cart')
+    mountingDiv
   );
 };
 
