5663aaca50124786c99fdcba42e4a8a715435684
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-03-19 00:18:18 -0400

    remove unused exports

--- skypack.d.ts
+++ skypack.d.ts
@@ -1,12 +1,5 @@
 declare module 'https://cdn.skypack.dev/react@v17.0.1?min' {
-  export {
-    useState,
-    useEffect,
-    Dispatch,
-    SetStateAction,
-    FC,
-    ChangeEvent
-  } from 'react';
+  export { FC, useState, useEffect } from 'react';
 }
 
 declare module 'https://cdn.skypack.dev/react-dom@v17.0.1?min' {
