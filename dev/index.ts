fd19494e87e9ff199cdcbf514bcb863c19fa4dbc
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-21 08:54:27 -0400

    add product modal

--- dev/index.ts
+++ dev/index.ts
@@ -1,5 +1,9 @@
 import mountCart from '../src/index';
 import { mountToast, showToast } from '@qbcart/toast';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import mountProductModal from '@qbcart/eshop-product-modal';
 
 mountToast();
+mountProductModal();
 mountCart(showToast);
