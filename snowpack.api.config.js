a18d85f085cef30bcf9bc9852364eb69eae7755c
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-20 23:34:59 -0400

    wire up api proxy

---
+++ snowpack.api.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+const httpProxy = require('http-proxy');
+const proxy = httpProxy.createServer({ target: 'http://localhost:7080' });
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: { url: '/src', resolve: true, static: false },
+    dev: { url: '/', resolve: true, static: false }
+  },
+  packageOptions: {
+    source: 'remote'
+  },
+  devOptions: {},
+  buildOptions: {},
+  plugins: [
+    ['@snowpack/plugin-sass', { compilerOptions: { loadPath: 'node_modules' } }]
+  ],
+  routes: [
+    {
+      src: '/data/.*',
+      dest: (req, res) => {
+        // remove /data prefix
+        req.url = req.url.replace(/^\/data/, '');
+
+        proxy.web(req, res);
+      }
+    }
+  ]
+};
