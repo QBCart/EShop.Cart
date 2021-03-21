1b93a76cce3fa411b590018e79cbb402b15e5e7d
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-21 00:03:32 -0400

    clean up

--- dev/index.ts
+++ dev/index.ts
@@ -2,5 +2,4 @@ import mountCart from '../src/index';
 import { mountToast, showToast } from '@qbcart/toast';
 
 mountToast();
-//mountCart('http://localhost:7070/cart'); // gateway
-mountCart(showToast, 'http://localhost:7078'); // direct api
+mountCart(showToast);
